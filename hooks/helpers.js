679ae507549c841b12fe7e11382cf3f9adf09be0
kayzer <dasilvawil218@gmail.com>
2023-01-17 21:55:23 +0100

    modif de recherche des donné

--- hooks/helpers.js
+++ hooks/helpers.js
@@ -1,33 +1,40 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = 'http://localhost:3000/';
 
 // concessionnaires qui font dans la location
 export const get_concessionnaires_location = async () => {
-  const response = await axios.get(`${BASE_URL}api/location/dealers`);
-  return response.data;
+  const response = await fetch(`${BASE_URL}api/location/dealers`);
+  const json = await response.json();
+  return json;
 };
 
 // concessionnaires qui font dans la vente
 export const get_concessionnaires_vente = async () => {
-  const response = await axios.get(`${BASE_URL}api/vente/dealers`);
-  return response.data;
+  const response = await fetch(`${BASE_URL}api/vente/dealers`);
+  return response.json();
 };
 
 // récuperer un concssionnaire spécifique
 export const get_dealer = async (target) => {
-  const response = await axios.get(`${BASE_URL}api/dealers/${target}`);
-  return response.data;
+  const response = await fetch(
+    `${BASE_URL}api/dealers/${target}`
+  );
+  return response.json();
 };
 
 // particuliers qui font dans la location
 export const get_particuliers_location = async () => {
-  const response = await axios.get(`${BASE_URL}api/location/individuals/`);
-  return response.data;
+  const response = await fetch(
+    `${BASE_URL}api/location/individuals/`
+  );
+  return response.json();
 };
 
 // particuliers qui font dans la vente
 export const get_particuliers_vente = async () => {
-  const response = await axios.get(`${BASE_URL}api/vente/individuals/`);
-  return response.data;
+  const response = await fetch(
+    `${BASE_URL}api/vente/individuals/`
+  );
+  return response.json();
 };
